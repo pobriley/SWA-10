3c268fffa700c69d9819c55c9710050e081ae6a7
pobriley <59428856+pobriley@users.noreply.github.com>
2021-09-06 21:51:11 +0700

    Add files via upload

---
+++ SWA-10/src/SingletionCilent.java
@@ -0,0 +1,7 @@
+
+public class SingletionCilent {
+	public static void main(String [] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton.getDescription());
+    }
+}
